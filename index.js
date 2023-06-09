6209bd43e21c6fe1eda64f0dcd5822c93ecfd78c
Mamun-Ur-Rashid <engmamun656@gmail.com>
2023-06-09 07:30:11 +0600

    verify middleware create

--- index.js
+++ index.js
@@ -2,12 +2,30 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require ('express');
 const app = express();
 require('dotenv').config();
+var jwt = require('jsonwebtoken');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
+
+// verify jwt
+const verifyJWT = (req, res, next) => {
+    const authorization = req.headers.authorization;
+    if(!authorization){
+      return res.status(401).send({error: true, message:'unauthorized access'})
+    }
+    // baerer token
+    const token = authorization.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+      if(err){
+        return res.status(401).send({error: true, message: 'unauthorized access'})
+      }
+      req.decoded = decoded;
+      next();
+    })
+  }
 
 
 
@@ -28,7 +46,12 @@ async function run() {
     await client.connect();
 const studentCollection = client.db("yogaDb").collection('students');
 
-
+// jwt token
+app.post('/jwt', (req, res) => {
+    const student = req.body;
+    const token = jwt.sign(student, process.env.access_token_secret, { expiresIn: '1h'})
+    res.send({token});
+})
 // students collection api
 app.get('/students', async (req, res) => {
     const result = await studentCollection.find().toArray();
